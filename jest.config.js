aedfeffc608ade6b309b43e900784bc2a994e51a
hasan-py <hasan29351@gmail.com>
2022-07-03 21:35:53 +0600

    Setup: Initialize Project

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  roots: ["<rootDir>/src"],
+  moduleFileExtensions: [
+    "tsx",
+    "ts",
+    "web.js",
+    "js",
+    "web.ts",
+    "web.tsx",
+    "json",
+    "web.jsx",
+    "jsx",
+    "node",
+  ],
+  watchPlugins: [
+    "jest-watch-typeahead/filename",
+    "jest-watch-typeahead/testname",
+  ],
+  resetMocks: true,
+};
---
+++ src/__test__/App.test.tsx
@@ -0,0 +1,3 @@
+describe("Hello World Test!!", () => {
+  it("Hello World Test!!", () => {});
+});
